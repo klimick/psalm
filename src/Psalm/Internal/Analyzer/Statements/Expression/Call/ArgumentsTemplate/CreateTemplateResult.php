223d07fdc333aaa7f160ece3319b4f80838d227d
klimick <klimichkartorgnusov@gmail.com>
2024-05-02 22:44:01 +0300

    Refactor a bit

--- src/Psalm/Internal/Analyzer/Statements/Expression/Call/ArgumentsTemplate/CreateTemplateResult.php
+++ src/Psalm/Internal/Analyzer/Statements/Expression/Call/ArgumentsTemplate/CreateTemplateResult.php
@@ -9,16 +9,15 @@ use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Name;
 use Psalm\Codebase;
 use Psalm\Context;
-use Psalm\Internal\Analyzer\FunctionLikeAnalyzer;
 use Psalm\Internal\Analyzer\Statements\Expression\Call\ClassTemplateParamCollector;
 use Psalm\Internal\Analyzer\StatementsAnalyzer;
-use Psalm\Internal\Analyzer\TraitAnalyzer;
 use Psalm\Internal\MethodIdentifier;
 use Psalm\Internal\Type\TemplateResult;
 use Psalm\Internal\Type\TemplateStandinTypeReplacer;
 use Psalm\Storage\ClassLikeStorage;
 use Psalm\Storage\MethodStorage;
 use Psalm\Type\Atomic;
+use Psalm\Type\Atomic\TNamedObject;
 use Psalm\Type\Atomic\TTemplateParam;
 use Psalm\Type\Union;
 
@@ -49,27 +48,6 @@ final class CreateTemplateResult
         }
 
         $method_name_lc = strtolower($method_storage->cased_name);
-        $self_call = !$statements_analyzer->isStatic() && $class_storage->name === $context->self;
-
-        /** @psalm-suppress TypeDoesNotContainType */
-        if ($self_call && false) {
-            $trait_lower_bounds = self::getTraitLowerBounds(
-                static_class_storage: $class_storage,
-                statements_analyzer: $statements_analyzer,
-                lhs_type_part: $lhs_type_part,
-                method_name_lc: $method_name_lc,
-            );
-
-            if ($trait_lower_bounds !== null) {
-                return new CollectedArgumentTemplates(
-                    template_types: array_merge(
-                        $class_storage->template_types ?? [],
-                        $method_storage->template_types ?? [],
-                    ),
-                    lower_bounds: $trait_lower_bounds,
-                );
-            }
-        }
 
         $class_lower_bounds = ClassTemplateParamCollector::collect(
             codebase: $codebase,
@@ -79,7 +57,7 @@ final class CreateTemplateResult
             static_class_storage: $class_storage,
             method_name: $method_name_lc,
             lhs_type_part: $lhs_type_part,
-            self_call: $self_call,
+            self_call: !$statements_analyzer->isStatic() && $class_storage->name === $context->self,
         ) ?? [];
 
         return new CollectedArgumentTemplates(
@@ -94,7 +72,9 @@ final class CreateTemplateResult
                         self_bounds: $class_lower_bounds,
                     )
                     : $class_lower_bounds,
-                self::getIfThisIsTypeLowerBounds($statements_analyzer, $method_storage, $lhs_type_part),
+                $lhs_type_part instanceof TNamedObject
+                    ? self::getIfThisIsTypeBounds($statements_analyzer, $method_storage, $lhs_type_part)
+                    : [],
             ),
         );
     }
@@ -138,49 +118,12 @@ final class CreateTemplateResult
     }
 
     /**
-     * @param lowercase-string $method_name_lc
-     * @return ?array<string, non-empty-array<string, Union>>
-     */
-    private static function getTraitLowerBounds(
-        ClassLikeStorage $static_class_storage,
-        StatementsAnalyzer $statements_analyzer,
-        Atomic $lhs_type_part,
-        string $method_name_lc,
-    ): ?array {
-        $parent_source = $statements_analyzer->getSource();
-
-        if (!$parent_source instanceof FunctionLikeAnalyzer) {
-            return null;
-        }
-
-        $grandparent_source = $parent_source->getSource();
-
-        if (!$grandparent_source instanceof TraitAnalyzer) {
-            return null;
-        }
-
-        $codebase = $statements_analyzer->getCodebase();
-        $trait_storage = $codebase->classlike_storage_provider->get($grandparent_source->getFQCLN());
-
-        return isset($trait_storage->methods[$method_name_lc])
-            ? ClassTemplateParamCollector::collect(
-                codebase: $codebase,
-                class_storage: $trait_storage,
-                static_class_storage: $static_class_storage,
-                method_name: $method_name_lc,
-                lhs_type_part: $lhs_type_part,
-                self_call: true,
-            )
-            : null;
-    }
-
-    /**
      * @return array<string, array<string, Union>>
      */
-    private static function getIfThisIsTypeLowerBounds(
+    private static function getIfThisIsTypeBounds(
         StatementsAnalyzer $statements_analyzer,
         MethodStorage $method_storage,
-        Atomic $lhs_type_part,
+        TNamedObject $lhs_type_part,
     ): array {
         if ($method_storage->if_this_is_type === null) {
             return [];
